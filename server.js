6ed711816bad82aa52fc6dbf5987eb4e27b6a4e5
CJ Davis <cj@cjdavis.me>
2017-01-28 13:19:46 -0500

    Allow Authorization request header

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ server.use(bodyParser.urlencoded({ extended: true })); // for parsing applicatio
 server.use(bodyParser.text());
 server.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept", "Authorization");
   next();
 });
 
